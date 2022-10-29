c3baf7a0038ce3c302d45b06df25f7a1b343a946
jeremyagnz <jeremyagnz@gmail.com>
2022-10-28 23:05:54 -0400

    Adding Conditionals

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
+  private _historial: string[] = [];
 
-  private _historial:string[] = []
-
-  get historial(){
-    return [...this._historial]
+  get historial() {
+    return [...this._historial];
   }
 
-  buscarGifs(query:string){
-    this._historial.unshift( query );
+  buscarGifs(query: string) {
+    query = query.trim().toLowerCase();
+
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+    }
+    
+    this._historial = this._historial.splice(0, 10);
     console.log(this._historial);
   }
 
-  constructor() { }
+  constructor() {}
 }
