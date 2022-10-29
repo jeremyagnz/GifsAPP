567f60eda3787c157fa87b45225c8a2367deac22
jeremyagnz <jeremyagnz@gmail.com>
2022-10-28 21:27:34 -0400

    Using Service To store data and use it

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,20 +1,20 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.css']
+  styleUrls: ['./busqueda.component.css'],
 })
-export class BusquedaComponent  {
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+  constructor(private gifsService: GifsService) {}
 
-  buscar(){
+  buscar() {
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    this.gifsService.buscarGifs(valor);
 
     this.txtBuscar.nativeElement.value = '';
   }
-
-
 }
